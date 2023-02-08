3f66929f1dbba481e9f1b4c072ebff2656995068
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-02-08 20:19:03 +0100

    pf

--- Deconstruction_Example/Person.cs
+++ Deconstruction_Example/Person.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Deconstruction_Example
+﻿namespace Deconstruction_Example
 {
     public class Person
     {
